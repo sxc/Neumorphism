ca60d37ccd305e5162bb9a5ea35807863eb6543a
sxc <qiuyequ@icloud.com>
2021-05-13 11:43:34 +0800

    Initial Commit

---
+++ Shared/NeumorphismApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeumorphismApp.swift
+//  Shared
+//
+//  Created by Xiaochun Shen on 2021/5/13.
+//
+
+import SwiftUI
+
+@main
+struct NeumorphismApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
