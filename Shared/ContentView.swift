d7bb525add548b3bbc5a970eb5b326c0a5029edb
sxc <qiuyequ@icloud.com>
2021-05-13 14:53:17 +0800

    Initial the project

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,13 +9,24 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+           Rectangle()
+            .fill(Color("Element"))
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .edgesIgnoringSafeArea(.all)
+            RoundedRectangle(cornerRadius: 20)
+                .fill(Color("Element"))
+                .frame(width: 300, height: 180)
+                .shadow(color: Color("LightShadow"), radius: 8, x: -8, y: -8)
+                .shadow(color: Color("DarkShadow"), radius: 8, x: 8, y: 8)
+        
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            
     }
 }
